5018ee19c1569acea4fba639c674690673af0f63
Mathieu FERRE <contact@mathieuferre.fr>
2017-09-12 19:01:32 +0000

    Apply fixes from StyleCI

--- src/CalendarController.php
+++ src/CalendarController.php
@@ -2,33 +2,30 @@
 
 namespace Arkhas\Calendar;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Calendar;
-use Illuminate\Support\Facades\Session;
-
-
-use App\Http\Requests;
+use Illuminate\Http\Request;
 
 class CalendarController extends Controller
 {
-    public function index($year='', $month='')
+    public function index($year = '', $month = '')
     {
-    	$calendar = Calendar::generate($year,$month);
-    	return $calendar;
+        $calendar = Calendar::generate($year, $month);
+
+        return $calendar;
     }
 
     public function demo(Request $request)
-    {	
-    	$events = array(
-		    '2016/5/3',
-		    '2016/5/5',
-		    '2016/5/11',
-		    '2016/6/16',
-		    '2016/6/28',
-		);
-    	$calendar = Calendar::generate('', '', $events);
+    {
+        $events = [
+            '2016/5/3',
+            '2016/5/5',
+            '2016/5/11',
+            '2016/6/16',
+            '2016/6/28',
+        ];
+        $calendar = Calendar::generate('', '', $events);
 
-    	return view('calendar::demo', compact('calendar'));
+        return view('calendar::demo', compact('calendar'));
     }
 }
